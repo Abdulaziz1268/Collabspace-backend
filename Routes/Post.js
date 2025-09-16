3e5d7a3486cc583f7a10f0cf12ef8221b364bac9
Abdulaziz1268 <abdumh1268@gmail.com>
2025-09-16 03:04:25 +0300

    add file upload handling to post creation and configure CORS settings

--- Routes/Post.js
+++ Routes/Post.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { authentication } from "../Middlewares/auth.js"
 import { createPost, getAllPosts } from "../Controllers/Post.js"
+import upload from "../Config/Storage.js"
 
 const router = express.Router()
 
-router.post("/createPost", authentication, createPost)
+router.post("/createPost", authentication, upload.single("media"), createPost)
 router.get("/getPosts", authentication, getAllPosts)
 
 export default router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import cors from "cors"
 import dotenv from "dotenv"
 import express from "express"
+import { fileURLToPath } from "url"
+import path from "path"
 
 import authRoutes from "./Routes/auth.js"
 import userRoutes from "./Routes/user.js"
@@ -12,8 +14,17 @@ dotenv.config()
 connectDB()
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+)
 
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
