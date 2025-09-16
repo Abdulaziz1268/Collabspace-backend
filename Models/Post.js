675774360597eaf438754241f6eb0d17a1fc5b0a
Abdulaziz1268 <abdumh1268@gmail.com>
2025-09-16 03:03:39 +0300

    add mediaUrl to Post schema and imageUrl to User schema

--- Models/Post.js
+++ Models/Post.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 const postSchema = new mongoose.Schema(
   {
     content: { type: String, required: true },
+    mediaUrl: { type: String },
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
