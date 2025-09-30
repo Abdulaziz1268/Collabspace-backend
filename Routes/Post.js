0e8b706ac6699440f5addad0d880c10b13977d1a
Abdulaziz1268 <abdumh1268@gmail.com>
2025-09-30 04:19:55 +0300

    added edit endpoint to the post routes

--- Routes/Post.js
+++ Routes/Post.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { authentication } from "../Middlewares/auth.js"
-import { createPost, getAllPosts } from "../Controllers/Post.js"
+import { createPost, getAllPosts, likePost } from "../Controllers/Post.js"
 import upload from "../Config/Storage.js"
 
 const router = express.Router()
 
 router.post("/createPost", authentication, upload.single("media"), createPost)
 router.get("/getPosts", authentication, getAllPosts)
+router.put("/:id/like", authentication, likePost)
 
 export default router
