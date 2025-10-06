938006bcc240b6924cfa1d2a4ed3b816faa3417e
Abdulaziz1268 <abdumh1268@gmail.com>
2025-10-06 19:02:01 +0300

    added new routes to user

--- Routes/user.js
+++ Routes/user.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { profile } from "../Controllers/user.js"
+import { getUser, profile } from "../Controllers/user.js"
 import { authentication } from "../Middlewares/auth.js"
 
 const router = express.Router()
 
 router.get("/profile", authentication, profile)
+router.get("/getUser/:id", authentication, getUser)
+router.post("/editProfile/:id", authentication, editProfile)
 
 export default router
