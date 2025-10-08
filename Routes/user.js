22d577fcb0b4ec2ebd5aa31843c2a42bb5fabacd
Abdulaziz1268 <abdumh1268@gmail.com>
2025-10-08 21:40:32 +0300

    feat: add image upload handling to editProfile route

--- Routes/user.js
+++ Routes/user.js
@@ -1,11 +1,17 @@
 import express from "express"
 import { editProfile, getUser, profile } from "../Controllers/user.js"
 import { authentication } from "../Middlewares/auth.js"
+import upload from "../Config/Storage.js"
 
 const router = express.Router()
 
 router.get("/profile", authentication, profile)
 router.get("/getUser/:id", authentication, getUser)
-router.post("/editProfile/:id", authentication, editProfile)
+router.post(
+  "/editProfile/:id",
+  authentication,
+  upload.single("imageUrl"),
+  editProfile
+)
 
 export default router
